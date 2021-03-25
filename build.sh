a074cb4fe0fd182604b53ecfa1de12d7e02028e0
KoffeinFlummi <koffeinflummi@protonmail.com>
2021-03-25 13:38:58 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 
 cd rust
 
+# Needed for compiling C++ in sealpir-rust and submodules
 export PATH=$PATH:/opt/android-sdk/ndk/21.0.6113669/toolchains/llvm/prebuilt/linux-x86_64/bin
 
 sed -i 's/^#crate-type/crate-type/' Cargo.toml
 
-rustup run nightly cargo build --target aarch64-linux-android --release
-rustup run nightly cargo build --target armv7-linux-androideabi --release
+cargo build --target aarch64-linux-android --release
+cargo build --target armv7-linux-androideabi --release
 
 sed -i 's/^crate-type/#crate-type/' Cargo.toml
